6adc1c1147958daebe3fd7930148b37f6ad4c1e3
dzCazador <ctestaseca@hotmail.com>
2024-08-20 20:20:50 -0300

    Se agrego JWT y expreesValidator

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
 const sequelize = require('./config/database');
 const productoRoutes = require('./routes/productoRoutes');
 
@@ -12,12 +13,39 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(morgan());
 app.use(express.json());
+app.use(express.static('public'));
 
 // Rutas
-app.use('/productos', productoRoutes);
+app.use('/productos',validarToken, productoRoutes);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/login.html');
+});
+
+app.post('/auth', (req, res) => {
+    const { username, password } = req.body;
+    const user = {username: username, password: password}
+    const accessToken = generarToken(user);
+    res.status(200).json({accessToken: accessToken,message:'Acceso Correcto'})
+
+});
+
+function generarToken(user) {
+    // Implementación del generador de token
+    return jwt.sign(user, process.env.SECRET_KEY, {expiresIn: '5m'})    
+}
+
+function validarToken(req, res, next) {
+    const token = req.headers['authorization'];
+    if (!token) return res.status(401).json({ message: 'Token no proporcionado' });
+    jwt.verify(token, process.env.SECRET_KEY,(error,user) => {
+        if (error) return res.status(403).json({ message: 'Token inválido' });
+        next();
+    });
+}
 
 
 // Iniciar el servidor
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -1,4 +1,5 @@
 const objProducto = require('../models/productoModel');
+const { body, validationResult } = require('express-validator');
 
 
 // Controlador para obtener todos los Productos
@@ -39,59 +40,74 @@ exports.getProductoById = async (req, res) => {
 };
 
 // Controlador para crear un nuevo Producto
-exports.createProducto = async (req, res) => {
-    const { nombre, precio, cantidad, categoria } = req.body;
+exports.createProducto = [
+    body('nombre').notEmpty().withMessage('El nombre es requerido').trim().escape(),
+    body('precio').isFloat({ min: 0 }).withMessage('El precio debe ser un número positivo'),
+    body('cantidad').isInt({ min: 0 }).withMessage('La cantidad debe ser un entero positivo'),
+    body('categoria').notEmpty().withMessage('La categoría es requerida').trim().escape(),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { nombre, precio, cantidad, categoria } = req.body;
 
+        try {
+            // Crear nuevo Producto
+            const nuevoProducto = await objProducto.create({
+                nombre,
+                precio,
+                cantidad,
+                categoria
+            });
 
-    // Validar datos de entrada
-    if (!nombre || !precio || !cantidad || !categoria ) {
-        return res.status(400).json({ error: 'Faltan datos requeridos' });
+            // Responder con el Producto creado
+            res.status(201).json(nuevoProducto);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Error al crear el Producto' });
+        }
     }
-
-    try {
-        // Crear nuevo Producto
-        const nuevoProducto = await objProducto.create({
-            nombre,
-            precio,
-            cantidad,
-            categoria
-        });
-
-        // Responder con el Producto creado
-        res.status(201).json(nuevoProducto);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Error al crear el Producto' });
-    }
-};
+]
 
 // Controlador para actualizar un Producto
-exports.updateProducto = async (req, res) => {
-    const { id } = req.params;
-    const { nombre, precio, cantidad, categoria } = req.body;
+exports.updateProducto = [
+    body('nombre').optional().notEmpty().withMessage('El nombre no puede estar vacío').trim().escape(),
+    body('precio').optional().isFloat({ min: 0 }).withMessage('El precio debe ser un número positivo'),
+    body('cantidad').optional().isInt({ min: 0 }).withMessage('La cantidad debe ser un entero positivo'),
+    body('categoria').optional().notEmpty().withMessage('La categoría no puede estar vacía').trim().escape(),
 
-    try {
-        const Producto = await objProducto.findByPk(id);
-
-        if (!Producto) {
-            return res.status(404).json({ error: 'Producto no encontrado' });
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
+        const { id } = req.params;
+        const { nombre, precio, cantidad, categoria } = req.body;
 
-        Producto.nombre = nombre || Producto.nombre;
-        Producto.precio = precio || Producto.precio;
-        Producto.cantidad = cantidad || Producto.cantidad;
-        Producto.categoria = categoria || Producto.categoria;
+        try {
+            const Producto = await objProducto.findByPk(id);
 
-        await Producto.save();
-        res.status(200).json(Producto);
-    } catch (error) {
-        if (error.name === 'SequelizeUniqueConstraintError') {
-            return res.status(409).json({ error: 'El DNI o el email ya están en uso' });
+            if (!Producto) {
+                return res.status(404).json({ error: 'Producto no encontrado' });
+            }
+
+            Producto.nombre = nombre || Producto.nombre;
+            Producto.precio = precio || Producto.precio;
+            Producto.cantidad = cantidad || Producto.cantidad;
+            Producto.categoria = categoria || Producto.categoria;
+
+            await Producto.save();
+            res.status(200).json(Producto);
+        } catch (error) {
+            if (error.name === 'SequelizeUniqueConstraintError') {
+                return res.status(409).json({ error: 'El DNI o el email ya están en uso' });
+            }
+            console.error(error);
+            res.status(500).json({ error: 'Error al actualizar el Producto' });
         }
-        console.error(error);
-        res.status(500).json({ error: 'Error al actualizar el Producto' });
     }
-};
+]
 
 // Controlador para eliminar un Producto
 exports.deleteProducto = async (req, res) => {
